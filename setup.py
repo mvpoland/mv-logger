dc4b10699d170e39498a4a1c20c1c5cf3eaee4bc
meverone <meverone@gmail.com>
2019-10-04 09:33:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(
     name="mv-logger",
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     author_email='kontakt@mobilevikings.pl',
     author='Mobile Vikings PL',
